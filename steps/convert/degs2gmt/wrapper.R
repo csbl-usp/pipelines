bb15ae595ae04977f1923c3d7ac2b8fddaf9d93b
Lucas Cardozo <lucasecardozo@gmail.com>
2018-04-12 20:19:58 -0300

    documentation

--- steps/convert/degs2gmt/wrapper.R
+++ steps/convert/degs2gmt/wrapper.R
@@ -1,21 +1,32 @@
-# Subset table
-subSetTable <- function(tb, gene_col, logfc_col,p_value_col, p_value, logfc, nm_col) {
-    head(tb)
+#' Extract up and down regulated genes from DEGs table.
+#' 
+#' @param degs DataFrame with differentially expressed genes.
+#' @param gene_col Gene column name.
+#' @param fc_col Fold change column name.
+#' @param pv_col P-value column name.
+#' @param comp_col Comparisons column name.
+#' @param pv_cutoff P-value cutoff.
+#' @param fc_cutoff Fold change cutoff.
+#' @return List of up and down regulated genes for each comparison.
+degs2list <- function(tb, gene_col='gene', fc_col='logfc', 
+                        pv_col='adjpval', comp_col='comparison',
+                        pv_cutoff=0.1, fc_cutoff=0) {
     tb <- tb[which(tb[[gene_col]] != ""), ]
     up <- tb[which(tb[[logfc_col]] > logfc & tb[[p_value_col]] < p_value), gene_col]
     down <- tb[which(tb[[logfc_col]] < -logfc & tb[[p_value_col]] < p_value), gene_col]
     name <- unique(tb[[nm_col]])
-    nm <- c(paste0(name, "_up"), paste0(name, "_down"))
     results <- list(up, down)
-    names(results) <- nm
+    names(results) <- c(paste0(name, "_up"), paste0(name, "_down"))
     return(results)
 }
 
-# List to dataframe
-list2Gmt <- function(tb) {
-    print(class(tb))
-    max_ln <- max(sapply(tb, length))
-    results <- lapply(tb, function(deg) {
+#' Turns degs list to .GMT format.
+#' 
+#' @param degs_list List of gene sets.
+#' @return DataFrame in GMT format.
+list2gmt <- function(degs_list) {
+    max_ln <- max(sapply(degs_list, length))
+    results <- lapply(degs_list, function(deg) {
         dif <- max_ln - length(deg)
         result <- c(deg, rep("", dif))
     })
@@ -27,37 +38,21 @@ list2Gmt <- function(tb) {
 # Get file path and parameters
 degs_file <- snakemake@input[["degs"]]
 params <- snakemake@params
-params <- NULL
-if (is.null(params)) {
-    gene_col <- "HGNC.symbol"
-    logfc_col <- "log2FoldChange"
-    p_value_col <- "padj"
-    p_value <- 0.1
-    logfc <- 0
-    nm_col <- "group"
-} else {
-    gene_col <- params[["gene_col"]]
-    logfc_col <- params[["logfc_col"]]
-    p_value_col <- params[["p_value_col"]]
-    p_value <- as.numeric(params[["p_value"]])
-    logfc <- as.numeric(params[["logfc"]])
-    nm_col <- params[["group"]]
-}
 
 # Read DEGs table and transform into GMT
-degs_file <- "~/projects/fabiopohl/zika/meta/results/DEGs/all_degs.csv"
 degs <- read.csv(file=degs_file, header=TRUE, stringsAsFactors=FALSE)
 
-gps <- unique(degs[, nm_col])
+gps <- unique(degs[, ifelse(is.null(params[["group"]], "group", params[["group"]]))])
 all_groups <- lapply(gps, function(gp){
-    mydf <- degs[which(degs$group == gp),]
-    up_down <- subSetTable(mydf, gene_col, logfc_col, p_value_col, p_value, logfc, nm_col)
+    df <- degs[which(degs$group == gp),]
+    params[["degs"]] <- df
+    up_down <- do.call(degs2list, params)
     return(up_down)
 })
 
 all_groups <- unlist(all_groups, recursive=F)
-all_groups <- all_groups[lapply(all_groups, function(x){length(x)})>0]
-gmt <- list2Gmt(all_groups)
+all_groups <- all_groups[lapply(all_groups, function(x){length(x)}) > 0]
+gmt <- list2gmt(all_groups)
 
 if (length(snakemake@output) > 0) {
     output <- snakemake@output[[1]]
